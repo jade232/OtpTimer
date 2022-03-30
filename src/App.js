f343ee68514bc176cd280b2e6c84c9220bbe21b8
jade232 <jabaraj002@gmail.com>
2022-03-30 18:28:43 +0530

    css done

--- src/App.js
+++ src/App.js
@@ -4,65 +4,101 @@ import ButtonComponent from './Components/ButtonComponent';
 import Grid from '@mui/material/Grid';
 import Link from '@mui/material/Link';
 import SignIn from './Components/SignIn';
+import { Button } from '@mui/material';
 
-
+import "./App.css"
 
 function App() {
 
-    const [ status, setStatus] = useState(0);
-    const [ inter, setInter] = useState();
-    const [ time, setTime] = useState({ ms:0, s:60, m:0, h:0});
-    const [ show, setShow] = useState(false);
+  // const [status, setStatus] = useState(0);
+  // const [inter, setInter] = useState();
+  // const [time, setTime] = useState({ ms: 0, s: 60, m: 0, h: 0 });
+  // const [show, setShow] = useState(false);
 
-    const SendOtp = () => {
-      run();
-      setStatus(1);
-      setInter(setInterval(run, 10));
+  // const SendOtp = () => {
+  //   run();
+  //   setStatus(1);
+  //   setInter(setInterval(run, 10));
+  // }
+
+  // var updatedMs = time.ms, updatedS = time.s, updatedM = time.m, updatedH = time.h;
+
+  // const run = () => {
+  //   if (updatedM === 60) {
+  //     updatedH++;
+  //     updatedM = 0;
+  //   }
+  //   if (updatedS === 0) {
+  //     updatedM++;
+  //     updatedS = 60;
+  //   }
+  //   if (updatedMs === 100) {
+  //     updatedS--;
+  //     updatedMs = 0;
+  //   }
+  //   updatedMs++;
+  //   return setTime({ ms: updatedMs, s: updatedS, m: updatedM, h: updatedH });
+  // };
+
+  // const ResendOtp = () => {
+  //   clearInterval(inter);
+  //   setStatus(0);
+  //   setTime({ ms: 0, s: 0, m: 0, h: 0 })
+  // };
+
+  // const resume = () => SendOtp();
+
+  const [count, setCount] = React.useState(10)
+  const [click, setClick] = React.useState(false)
+
+  // const runTimer = () => {
+
+  // }
+
+  const Start = () => {
+    if (click === true && count > 0) {
     }
+  }
 
-    var updatedMs = time.ms, updatedS = time.s, updatedM = time.m, updatedH = time.h;
 
-    const run = () => {
-      if(updatedM === 60){
-        updatedH++;
-        updatedM = 0;
-      }
-      if(updatedS === 0){
-        updatedM++;
-        updatedS = 60;
-      }
-      if(updatedMs === 100){
-        updatedS--;
-        updatedMs = 0;
-      }
-      updatedMs++;
-      return setTime({ms:updatedMs, s:updatedS, m:updatedM, h:updatedH});
-    };
+  // React.useEffect(() => {
 
-    const ResendOtp = () => {
-      clearInterval(inter);
-      setStatus(0);
-      setTime({ms:0, s:0, m:0, h:0})
-    };
+  //   if (!click) {
+  //     count > 0 && setTimeout(() => setCount(count - 1), 1000);
+  //   }
 
-    const resume = () => SendOtp();
-
+  // }, [count])
 
   return (
     <div className="App" >
-      <SignIn />
-      <Grid container direction="row" justifyContent="center" alignItems="center" sx={{ mt: 2}}>
-          <Link href="#"  onClick={() => setShow(!show)} variant="body2">
-              Forgot password?
-          </Link>
+      {/* <SignIn />
+      <Grid container direction="row" justifyContent="center" alignItems="center" sx={{ mt: 2 }}>
+        <Link href="#" onClick={() => setShow(!show)} variant="body2">
+          Forgot password?
+        </Link>
       </Grid>
       {
-      show ? (<Grid container direction="row" justifyContent="center" alignItems="center"  sx={{ mt: 3, mb: 2, gap: 4 }}>
-        <Grid><Timer time={time}/></Grid>
-          <Grid><ButtonComponent status={status} SendOtp={SendOtp} ResendOtp={ResendOtp} resume={resume}/></Grid>
-      </Grid>) : null
-      }
-      
+        show ? (<Grid container direction="row" justifyContent="center" alignItems="center" sx={{ mt: 3, mb: 2, gap: 4 }}>
+          <Grid><Timer time={time} /></Grid>
+          <Grid><ButtonComponent status={status} SendOtp={SendOtp} ResendOtp={ResendOtp} resume={resume} /></Grid>
+        </Grid>) : null
+      } */}
+      {/* <div className='btn-container'>
+        <Button className="btn-buddy">Start</Button>
+      </div> */}
+
+      <div className='ball-container'>
+        <div className='red-dot'></div>
+        <div className='green-dot'></div>
+        <div className='purple-dot'></div>
+        <div className='blue-dot'></div>
+      </div>
+
+
+      {/* <h4>Hello</h4> */}
+
+      {/* {count} */}
+
     </div>
   );
 }
